d52e5129fd69a668ebd67ad8738d85ad2e4cc82c
Neel Adwani <nprosofficial@gmail.com>
2021-10-09 08:59:20 +0000

    added the dataset

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ app = Flask('app')
 def hello_world():
   return 'Hello, World!'
 
-app.run(host='0.0.0.0',	debug=True,	port=8080)
+# app.run(host='0.0.0.0',	debug=True,	port=8080)
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import sklearn.metrics
+
+data = pd.read_csv("stonks.csv")
+print(data)
