a9dcecab5f7e6ff55d5d700cd7ca71c620d2c361
Neel Adwani <nprosofficial@gmail.com>
2021-10-10 04:55:18 +0000

    added calamities

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 import sklearn.metrics
 
 data = pd.read_csv("stonks.csv")
+calamities = pd.read_csv("disasters.csv")
 
 app = Flask('app', template_folder = 'templates', static_folder = 'static')
 
@@ -21,7 +22,7 @@ def hello_world():
 def fetch():
   stock = request.form.get('stock')
   data_stonks = data[(data["Index"] == stock)]
-  data_stonks = data_stonks["Date"]
+  data_stonks = [data_stonks["Date"], calamities["declaration_date"]]
   data_stonks_y = data[(data["Index"] == stock)]
   data_stonks_y = data_stonks_y["Open"]
   fig = px.line(data, x= data_stonks, y=data_stonks_y)
